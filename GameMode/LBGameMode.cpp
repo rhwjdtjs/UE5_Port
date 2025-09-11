3d621c480bf00e3ef623a1d2de17f6553699ded0
Ko jeong seon <qqq253222@naver.com>
2025-09-11 15:59:46 +0900

    시연 테스트 전 수정

--- GameMode/LBGameMode.cpp
+++ GameMode/LBGameMode.cpp
@@ -9,11 +9,11 @@ void ALBGameMode::PostLogin(APlayerController* NewPlayer) //플레이어 컨트롤러를 
 	Super::PostLogin(NewPlayer); //부모클래스의 PostLogin을 호출한다.
 
 	int32 NumberOfPlayer = GameState.Get()->PlayerArray.Num(); //게임 상태의 플레이어 배열의 개수를 가져온다.
-	if (NumberOfPlayer == 2) {
+	if (NumberOfPlayer == 3) {
 		UWorld* World = GetWorld();
 		if (World) {
 			bUseSeamlessTravel = true; //서버 트래블을 사용한다.
-			World->ServerTravel(FString("/Game/Maps/testingame?listen")); //서버 트래블을 통해 맵을 이동한다.
+			World->ServerTravel(FString("/Game/Maps/InGameMap?listen")); //서버 트래블을 통해 맵을 이동한다.
 		}
 	}
 }
